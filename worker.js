d53576591ee2d8a21526d4ee6040be957951316b
James Kyburz <james.kyburz@gmail.com>
2015-11-18 20:20:55 +0100

    Didn't have any audio data in callback fix

--- worker.js
+++ worker.js
@@ -1,5 +1,6 @@
 var speaker = require('speaker')()
 var net = require('net')
+var through = require('through2')
 var server = net.createServer(play)
 server.listen(0, running)
 
@@ -8,7 +9,13 @@ function running () {
 }
 
 function play (s) {
+  var buffer = through(pipe)
   s.on('error', console.error.bind(console))
   speaker.on('error', console.error.bind(console))
-  s.pipe(speaker)
+  s.pipe(buffer)
+  buffer.on('end', speaker.end.bind(speaker))
+  function pipe (data, enc, cb) {
+    speaker.write(data)
+    cb()
+  }
 }
