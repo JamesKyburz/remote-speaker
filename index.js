6ed53a7807393c0c2af033bfbf5a3293c945fb4a
James Kyburz <james.kyburz@gmail.com>
2016-02-07 16:14:08 +0100

    standard path fix

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var http = require('http')
 var net = require('net')
+var path = require('path')
 var fork = require('child_process').fork
 var worker
 
 http.createServer(function (q, r) {
   if (worker) worker.kill()
-  worker = fork(__dirname + '/worker.js')
+  worker = fork(path.join(__dirname, '/worker.js'))
   worker.on('message', play(q, r))
 }).listen(process.env.PORT | 9000)
 
